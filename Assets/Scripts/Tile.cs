e71cfc7ab91a73b179240625295606e304e6c0cf
Falco Schmitt <schmittf1990@yahoo.de>
2017-06-07 22:28:20 +0200

    Initial Commit
    Scripts:
    +Levelspawner
    +Gamemanager
    +Unit
    +Tile
    
    Units:
    +horse
    +Bear(80%)
    +Snake(40%)

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	[SerializeField]
+	private bool occupied;
+	[SerializeField]
+	private GameObject unit = null;
+	private int xGridPosition;
+	private int zGridPosition;
+
+	private Material defaultMaterial;
+
+	[SerializeField]
+	private List<GameObject> adjacentTiles;
+
+	// Use this for initialization
+	void Start () {
+		defaultMaterial = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Setup(int xGrid, int zGrid){
+		xGridPosition = xGrid;
+		zGridPosition = zGrid;
+		this.name = "Tile("+xGrid+")("+zGrid+")";
+	}
+		
+	public void SetAllAdjacentTiles(){
+		for (int i = -1; i <= 1; i+=2) {
+				if (GameObject.Find ("Tile("+(xGridPosition+i)+")("+zGridPosition+")") != null) {
+					//Debug.Log("Found: "+ "Tile("+(xGridPosition+i)+")("+(zGridPosition+j)+")");
+					adjacentTiles.Add(GameObject.Find ("Tile("+(xGridPosition+i)+")("+zGridPosition+")"));
+				}											
+		}
+		for (int j = -1; j <= 1; j+=2) {
+			if (GameObject.Find ("Tile("+xGridPosition+")("+(zGridPosition+j)+")") != null) {
+				//Debug.Log("Found: "+ "Tile("+(xGridPosition+i)+")("+(zGridPosition+j)+")");
+				adjacentTiles.Add(GameObject.Find ("Tile("+xGridPosition+")("+(zGridPosition+j)+")"));
+			}
+		}
+	}
+
+	public void Recolor(Material newMaterial){
+		GetComponent<Renderer> ().material = newMaterial;
+	}
+
+	public void ResetColor(){
+		GetComponent<Renderer> ().material = defaultMaterial;
+	}
+
+	public void SetUnit(GameObject unit){
+		this.unit = unit;
+		if (unit == null) {
+			occupied = false;
+		} else {
+			occupied = true;
+		}			
+	}
+
+
+
+
+	//Propertystuff (Autogenerating)
+	public List<GameObject> AdjacentTiles {
+		get {
+			return adjacentTiles;
+		}
+	}
+
+	public bool Occupied {
+		get {
+			return occupied;
+		}
+		set {
+			occupied = value;
+		}
+	}
+
+	public GameObject Unit {
+		get {
+			return unit;
+		}
+	}
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+
+	public int movementSpeed;
+	public int attackRange;
+	public int damage;
+	public int health;
+
+	private bool ownedByPlayer;
+	private GameObject currentTile;
+
+	private bool usedThisRound = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Movement
+	public void FindAllMovableTiles (Material movableMaterial)
+	{
+		Tile baseTile = this.CurrentTile.GetComponent<Tile> ();
+		List<GameObject> toColorTiles = new List<GameObject> ();
+		//Add all adjacentTiles to recolor list
+
+		foreach (GameObject tile in baseTile.AdjacentTiles) {
+			if(!tile.GetComponent<Tile>().Occupied){
+				toColorTiles.Add(tile);
+			}
+		}
+		//add adjacent tiles of all added tiles, excluding already added tiles => repeat equal to movementspeed
+		for (int i = 1; i < this.movementSpeed; i++) {
+			List<GameObject> tmpTiles = new List<GameObject> ();
+			foreach (GameObject tile in toColorTiles) {
+				foreach (GameObject checkTile in tile.GetComponent<Tile>().AdjacentTiles) {
+					if (!checkTile.GetComponent<Tile> ().Occupied && checkTile.name != baseTile.name) {
+						tmpTiles.Add (checkTile);
+					}
+				}
+			}
+			foreach (GameObject tile in tmpTiles) {
+				if (!toColorTiles.Exists (t => t.name == tile.name)) {
+					toColorTiles.Add (tile);
+				}
+			}							
+		}
+		foreach (GameObject tileGO in toColorTiles) {
+			tileGO.GetComponent<Tile> ().Recolor (movableMaterial);
+		}
+	}
+
+
+	//Attack
+
+
+
+
+	public void Setup(GameObject tile){
+		currentTile = tile;
+	}
+
+	public GameObject CurrentTile {
+		get {
+			return currentTile;
+		}
+	}
+
+	public bool OwnedByPlayer {
+		get {
+			return ownedByPlayer;
+		}
+		set {
+			ownedByPlayer = value;
+		}
+	}
+
+	public bool UsedThisRound {
+		get {
+			return usedThisRound;
+		}
+		set {
+			usedThisRound = value;
+		}
+	}
+
+}
